2132f8a671d8197a25e5bae0478742889e2bac8f
ellyzyaory <71363310+ellyzyaory@users.noreply.github.com>
2020-11-20 10:10:25 +0700

    Create hapax_legomenon.py

---
+++ hapax_legomenon.py
@@ -0,0 +1,21 @@
+import re
+
+def hapax(book):
+    file = open(book, 'r', encoding="utf8")
+    text = re.findall("\w+", file.read().lower())
+    words_used = {}
+    list = []
+
+    for word in text:
+        words_used.setdefault(word, 0)
+        if word in words_used:
+            words_used[word] += 1
+
+    for key in words_used.keys():
+        if words_used[key] == 1:
+            list.append(key)
+
+    return list
+
+if __name__ == "__main__":
+    print(hapax("C:\\Users\\Johanes\\Desktop\\wuthering_heights.txt"))
